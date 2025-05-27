e0660769c192564a7ceb3130a75bb314067610c7
GiHoon Jang <stb.jang@gmail.com>
2025-05-27 14:31:44 +0900

    Update Application.java

--- src/main/java/com/swapreporting/Application.java
+++ src/main/java/com/swapreporting/Application.java
@@ -1,13 +1,26 @@
-package com.swapreporting;
+#package com.swapreporting;
+
+#import org.springframework.boot.SpringApplication;
+#import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+#@SpringBootApplication
+#public class Application {
+
+#	public static void main(String[] args) {
+#		SpringApplication.run(Application.class, args);
+#	}
+
+#}
+package com.swapreport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+@EnableScheduling
+public class SwapReportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwapReportApplication.class, args);
+    }
 }
