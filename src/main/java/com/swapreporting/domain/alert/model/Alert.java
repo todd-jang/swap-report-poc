1c1b6bd6a277d8d38ce26ed668058bf65ec44595
GiHoon Jang <stb.jang@gmail.com>
2025-05-27 14:49:15 +0900

    Update Alert.java

--- src/main/java/com/swapreporting/domain/alert/model/Alert.java
+++ src/main/java/com/swapreporting/domain/alert/model/Alert.java
@@ -1,9 +1,49 @@
-package com.example.swapreport.domain.alert.model;
+
+package com.swapreporting.domain.alert.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Alert {
+
     private String tradeId;
     private String message;
     private AlertType type;
 
-    // 생성자, Getters, Setters
+    public Alert() {} // 기본 생성자
+
+    @JsonCreator
+    public Alert(
+            @JsonProperty("tradeId") String tradeId,
+            @JsonProperty("message") String message,
+            @JsonProperty("type") AlertType type) {
+        this.tradeId = tradeId;
+        this.message = message;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public String getTradeId() {
+        return tradeId;
+    }
+
+    public void setTradeId(String tradeId) {
+        this.tradeId = tradeId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public AlertType getType() {
+        return type;
+    }
+
+    public void setType(AlertType type) {
+        this.type = type;
+    }
 }
