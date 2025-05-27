f8093260156aa1c8da74681a5068336ffd124802
GiHoon Jang <stb.jang@gmail.com>
2025-05-27 14:15:58 +0900

    Create KafkaConfig.java

---
+++ src/main/java/com/swapreporting/config/KafkaConfig.java
@@ -0,0 +1,29 @@
+package com.example.swapreport.config;
+
+import com.example.swapreport.domain.alert.model.Alert;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, Alert> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Alert> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
