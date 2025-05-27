315af1ce4a761dd0c54bcf21bd56ab273b0ea002
GiHoon Jang <stb.jang@gmail.com>
2025-05-27 14:18:11 +0900

    Create Report.java

---
+++ src/main/java/com/swapreporting/domain/report/model/Report.java
@@ -0,0 +1,11 @@
+package com.example.swapreport.domain.report.model;
+
+import java.time.LocalDateTime;
+
+public class Report {
+    private String tradeId;
+    private LocalDateTime timestamp;
+    private String summary;
+
+    // 생성자, Getters, Setters
+}
