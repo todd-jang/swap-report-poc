5ca09c3aeac689a28bf62ee947b8387491a8380a
GiHoon Jang <stb.jang@gmail.com>
2025-05-27 14:12:24 +0900

    Create Alert.java

---
+++ src/main/java/com/swapreporting/domain/alert/model/Alert.java
@@ -0,0 +1,9 @@
+package com.example.swapreport.domain.alert.model;
+
+public class Alert {
+    private String tradeId;
+    private String message;
+    private AlertType type;
+
+    // 생성자, Getters, Setters
+}
